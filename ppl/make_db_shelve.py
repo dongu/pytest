e6c8a682273daa0903954c6a14f101bde0a44e7c
dongu <cygu73@126.com>
2016-05-27 22:51:45 +0800

    ok

---
+++ ppl/make_db_shelve.py
@@ -0,0 +1,9 @@
+import shelve
+
+from initdata import bob, sue
+
+db = shelve.open('people-shelve')
+db['bob'] = bob
+db['sue'] = sue
+
+db.close()
--- ppl/update_db_pickle.py
+++ ppl/update_db_pickle.py
@@ -1,4 +1,3 @@
-
 import  pickle
 
 dbfile = open('people-pickle', 'rb')
